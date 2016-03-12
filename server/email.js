1c637f7501c53cc4263a8a354816c3b8c493c73e
Nick Breaton <nick@breaton.com>
2016-03-12 09:50:17 -0500

    Complete /email endpoint.

--- server/email.js
+++ server/email.js
@@ -2,7 +2,7 @@ var AWS = require('aws-sdk');
 var fs = require('fs');
 var path = require('path');
 
-var ses;
+var ses, client; // AWS
 var html;
 
 debug();
@@ -10,39 +10,82 @@ configure();
 content();
 
 module.exports = function (req, res) {
+   // ensure matching is correct
+   req.params.email = req.params.email.toLowerCase();
+
    // check email for UNCC address
    var isValidEmail = req.params.email.match(/^[a-zA-Z]+@uncc\.edu$/);
 
    // handle unathorized
    if (!isValidEmail) return res.sendStatus(401);
 
-   var code = 'sada89y113ewdsadas';
+   // generate random code
+   var code = Math.random().toString(36).substring(2);
 
-   ses.sendEmail({
-      Source: 'Niner Miners <whitelist@ninerminers.com>',
-      Destination: {
-         ToAddresses: [ req.params.email ]
-      },
-      Message: {
-         Subject: {
-            Data: 'Whitelist Confirmation'
+   saveToDatabase();
+
+   function saveToDatabase () {
+      var username_old;
+
+      client.query({
+         TableName: 'whitelist',
+         KeyConditionExpression: "#email = :email",
+         ExpressionAttributeNames:{
+            "#email": "email"
          },
-         Body: {
-            Html: {
-               Data: html.replace(/@code/g, code)
+         ExpressionAttributeValues: {
+            ":email": req.params.email
+         }
+      }, (err, data) => {
+         // hande error
+         if (err) return res.sendStatus(500);
+
+         // save old username if user already exists
+         if (data.Count) username_old = data.Items[0].username;
+         
+         client.put({
+            TableName: 'whitelist',
+            Item: {
+               email: req.params.email,
+               first: 'User',
+               last: 'Smith',
+               username: 'an_mc_user',
+               username_old,
+               verification: code
+            }
+         }, (err) => {
+            err ? res.sendStatus(500) : sendEmail();
+         });
+      });
+   }
+
+   function sendEmail () {
+      ses.sendEmail({
+         Source: 'Niner Miners <whitelist@ninerminers.com>',
+         Destination: {
+            ToAddresses: [ req.params.email ]
+         },
+         Message: {
+            Subject: {
+               Data: 'Whitelist Confirmation'
+            },
+            Body: {
+               Html: {
+                  Data: html.replace(/@code/g, code)
+               }
             }
          }
-      }
-   }, (err) => {
-      console.log(err);
-      res.sendStatus(err ? 500 : 200)
-   });
+      }, (err) => {
+         res.sendStatus(err ? 500 : 200)
+      });
+   }
 
 }
 
 function configure () {
    AWS.config.region = 'us-east-1';
    ses = new AWS.SES({apiVersion: '2010-12-01'});
+   client = new AWS.DynamoDB.DocumentClient();
 }
 
 function content () {
